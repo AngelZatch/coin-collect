a3a1c877527b3dfbe49de751959fce66c260052f
AngelZatch <angelzatch@gmail.com>
2020-07-09 16:49:17 +0200

    feat: cursor movement

--- src/app.ts
+++ src/app.ts
@@ -9,6 +9,7 @@ document.body.appendChild(app.view)
 
 // TODO: Solve typing of this
 let bowsette
+let canBoost = true
 
 app.loader
     .add('bowsette', 'assets/bowsette.png')
@@ -21,96 +22,19 @@ function setup(resources) {
     bowsette.y = app.renderer.height / 2
     bowsette.anchor.x = 0.5
     bowsette.anchor.y = 0.5
-    bowsette.scale.x = 0.4
-    bowsette.scale.y = 0.4
-    let playerXMovement = 0, playerYMovement = 0
+    bowsette.scale.x = 0.1
+    bowsette.scale.y = 0.1
 
     app.stage.addChild(bowsette)
 
-    // Setup keyboard
-    let left = keyboard("ArrowLeft"),
-    up = keyboard("ArrowUp"),
-    right = keyboard("ArrowRight"),
-    down = keyboard("ArrowDown");
+    // Mouse control
+    window.addEventListener('mousemove', (event) => {
+        bowsette.x = Math.min(Math.max(0, event.clientX), app.view.width)
+        bowsette.y = Math.min(Math.max(0, event.clientY), app.view.height)
+    })
 
-    left.press = () => {
-        playerXMovement = -2
-    }
-
-    left.release = () => {
-        playerXMovement = 0
-    }
-
-    up.press = () => {
-        playerYMovement = -2
-    }
-
-    up.release = () => {
-        playerYMovement = 0
-    }
-
-    down.press = () => {
-        playerYMovement = 2
-    }
-
-    down.release = () => {
-        playerYMovement = 0
-    }
-
-    right.press = () => {
-        playerXMovement = 2
-    }
-
-    right.release = () => {
-        playerXMovement = 0
-    }
-
-    app.ticker.add(() => loop(playerXMovement, playerYMovement))
+    app.ticker.add(() => loop())
 }
 
-function loop(playerXMovement, playerYMovement) {
-    bowsette.x += playerXMovement;
-    bowsette.y += playerYMovement;
-    console.log('HEY')
-}
-
-function keyboard(value) {
-    const key = {
-        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-        value,
-        isDown: false,
-        isUp: true,
-        press: () => {
-            key.isDown = true
-            key.isUp = false
-        },
-        release: () => {
-            key.isDown = false
-            key.isUp = true
-        },
-        downHandler: (event) => {
-            if (event.key === key.value) {
-                if (key.isUp) { key.press() }
-                event.preventDefault()
-            }
-        },
-        upHandler: (event) => {
-            if (event.key === key.value) {
-                if (key.isUp) { key.release() }
-                event.preventDefault()
-            }
-        },
-        unsubscribe: () => {
-            window.removeEventListener("keydown", downListener)
-            window.removeEventListener("keyup", upListener)
-        }
-    };
-
-    const downListener = key.downHandler.bind(key);
-    const upListener = key.upHandler.bind(key);
-
-    window.addEventListener("keydown", downListener, false)
-    window.addEventListener("keyup", upListener, false)
-
-    return key;
+function loop() {
 }
---
+++ src/controllers/keyboard.controller.ts
@@ -0,0 +1,40 @@
+export const keyboard = (value) => {
+    const key = {
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+        value,
+        isDown: false,
+        isUp: true,
+        press: () => {
+            key.isDown = true
+            key.isUp = false
+        },
+        release: () => {
+            key.isDown = false
+            key.isUp = true
+        },
+        downHandler: event => {
+            if (event.code === key.value) {
+                if (key.isUp) { key.press() }
+                event.preventDefault()
+            }
+        },
+        upHandler: event => {
+            if (event.code === key.value) {
+                if (key.isUp) { key.release() }
+                event.preventDefault()
+            }
+        },
+        unsubscribe: () => {
+            window.removeEventListener("keydown", downListener)
+            window.removeEventListener("keyup", upListener)
+        }
+    }
+
+    const downListener = key.downHandler.bind(key)
+    const upListener = key.upHandler.bind(key)
+
+    window.addEventListener("keydown", downListener, false)
+    window.addEventListener("keyup", upListener, false)
+
+    return key
+}
